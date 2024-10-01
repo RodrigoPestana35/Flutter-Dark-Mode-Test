08351cb99f348d12b120b3cfed1d8a291fa79c20
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-01 21:25:52 +0100

    Refactor theme.dart to add color scheme for light and dark modes

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,8 +2,18 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    surface: Colors.deepPurple.shade500,
+    primary: Colors.deepPurple.shade300,
+    secondary: Colors.deepPurple.shade200,
+  ),
 );
 
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
+  colorScheme: ColorScheme.dark(
+    surface: Colors.deepPurple.shade900,
+    primary: Colors.deepPurple.shade700,
+    secondary: Colors.deepPurple.shade600,
+  ),
 );
