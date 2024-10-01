c34bc3c1713079370e97bb2252370e7985abe20b
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-01 21:40:01 +0100

    Refactor theme colors to use grey shades instead of deep purple

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -3,17 +3,17 @@ import 'package:flutter/material.dart';
 ThemeData lightMode = ThemeData(
   brightness: Brightness.light,
   colorScheme: ColorScheme.light(
-    surface: Colors.deepPurple.shade500,
-    primary: Colors.deepPurple.shade300,
-    secondary: Colors.deepPurple.shade200,
+    surface: Colors.grey.shade500,
+    primary: Colors.grey.shade300,
+    secondary: Colors.grey.shade200,
   ),
 );
 
 ThemeData darkMode = ThemeData(
   brightness: Brightness.dark,
   colorScheme: ColorScheme.dark(
-    surface: Colors.deepPurple.shade900,
-    primary: Colors.deepPurple.shade700,
-    secondary: Colors.deepPurple.shade600,
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade700,
+    secondary: Colors.grey.shade600,
   ),
 );
