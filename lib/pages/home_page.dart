ce3dc1d9ad7317b19f72200b0eec2e954fa00ad5
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-01 21:08:22 +0100

    Add Box component with color and child properties

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:blackthemetestapp/components/box.dart';
+import 'package:blackthemetestapp/components/button.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,6 +9,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[500],
+      body: Center(
+        child: Box(
+          child: Text('Box'),
+          color: Colors.deepPurple[300],
+        ),
+      ),
     );
   }
 }
